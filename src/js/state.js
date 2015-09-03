f97b04884ffc476057ab6c655c3d3f8a182222b9
willviles <will@willviles.com>
2015-09-03 14:24:49 +0100

    Handles hash state init with hash which doesn't correspond to a section on the page

--- src/js/state.js
+++ src/js/state.js
@@ -19,19 +19,51 @@
 
     init: function(imm) {
       this.imm = imm;
+      var that = this;
 
       this.baseUrl = window.location.href.split("#")[0];
       this.hash = window.location.href.split("#")[1];
 
+      // If no hash in URL, set section to first and do not proceed
       if (this.imm.setup.options.hashChange !== true) {
         this.setSection.call(this, 'first');
         return false;
       }
 
-      this.setSection.call(this);
+      // Hash defined
+      if (this.hash) {
 
-      var that = this;
+        var hashMatch = this.findSection.call(this, this.hash),
+            hashesMatched = hashMatch.length;
 
+         // If matches section
+        if (hashesMatched > 0) {
+
+          // If one section, go to it
+          if (hashesMatched === 1) {
+            this.setSection.call(this);
+
+          // If multiple sections, alert developer
+          } else {
+            this.imm.utils.log(this.imm, "State Error: More than one section referenced as '" + this.hash + "'");
+          }
+
+        // If it doesn't match
+        } else {
+
+          // Set to first section and log lack of match
+          this.setSection.call(this, 'first');
+          history.replaceState({}, "", this.baseUrl);
+          this.imm.utils.log(this.imm, "State Error: No section referenced as '" + this.hash + "'");
+        }
+
+      // No hash defined
+      } else {
+        this.setSection.call(this, 'first');
+
+      }
+
+      // Set up hash handling on section change
       this.imm.$elem.on('sectionChanged', function(e, d) {
         that.hashChange.call(that, d);
       });
@@ -39,21 +71,21 @@
       return this;
     },
 
+
     // Set Section
     ///////////////////////////////////////////////////////
 
     setSection: function(o) {
-      if (o === 'first' || !this.hash) {
+      var that = this;
+
+      if (o === 'first') {
         this.imm._currentSection = this.imm._sections[0];
         this.imm._sectionBelow = this.imm._sections[1];
-        return;
+      } else {
+        this.imm._currentSection = this.findSection.call(this, this.hash)[0];
+        this.imm._sectionBelow = this.imm._sections[this.imm._currentSection.scrollIndex + 1];
+        this.imm._sectionAbove = this.imm._sections[this.imm._currentSection.scrollIndex - 1];
       }
-
-      this.imm._currentSection = this.findSection.call(this, this.hash)[0];
-      this.imm._sectionBelow = this.imm._sections[this.imm._currentSection.scrollIndex + 1];
-      this.imm._sectionAbove = this.imm._sections[this.imm._currentSection.scrollIndex - 1];
-
-      var that = this;
 
       this.imm.$elem.on('immInit', function(e) {
         that.imm._scrollContainer.scrollTop(that.imm._currentSection.scrollOffset);
@@ -66,7 +98,6 @@
     ///////////////////////////////////////////////////////
 
     findSection: function(hash) {
-
       return this.imm._sections.filter(function(s) {
         return s.id === hash;
       });
