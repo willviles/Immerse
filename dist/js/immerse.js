f99065cf73712c42b054be659353e4da02cef7e7
willviles <will@willviles.com>
2015-09-11 08:56:52 +0100

    Updates version references to v1.0.23

--- dist/js/immerse.js
+++ dist/js/immerse.js
@@ -1,7 +1,7 @@
 /*
 Script Name: Immerse.js
 Description: Build immersive, media driven web experiences the easy way
-Version: 1.0.22
+Version: 1.0.23
 Author: Will Viles
 Author URI: http://vil.es/
 */
